ace6efd69801ca9ccea51dd5e309db7982ad1b6b
vaishakhikulkarni <vaishakhikulkarni@gmail.com>
2015-09-03 00:37:05 -0500

    Sort

---
+++ Assignment5_aF.java
@@ -0,0 +1,157 @@
+//package DAY1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+
+public class Assignment5_aF {
+
+	final static int n = 8000000;
+	private static long startTime;
+
+	public static <T extends Comparable<? super T>> void MergeSort(T[] A,
+			int p, int r, T[] B) {
+		if (p < r) {
+			int q = (p + r) >>> 1;
+			MergeSort(A, p, q, B);
+			MergeSort(A, q + 1, r, B);
+			Merge(A, p, q, r, B);
+		}
+	}
+
+	public static <T extends Comparable<? super T>> void Merge(T[] A, int p,
+			int q, int r, T[] B) {
+		for (int i = p; i <= r; i++)
+			B[i] = A[i];
+
+		int i = p;
+		int j = q + 1;
+		for (int k = p; k <= r; k++) {
+			if (j > r || (i <= q && (B[i].compareTo(B[j]) <= 0)))
+				A[k] = B[i++];
+			else
+				A[k] = B[j++];
+		}
+	}
+
+	public static <T extends Comparable<? super T>> void InsertionSort(T[] C,
+			int p, int r) {
+		for (int i = p; i <= r; i++) {
+
+			for (int j = i; j > p; j--) {
+				if (C[j].compareTo(C[j - 1]) < 0) {
+					T temp = C[j];
+					C[j] = C[j - 1];
+					C[j - 1] = temp;
+				} else
+					break;
+			}
+		}
+	}
+
+	public static <T extends Comparable<? super T>> void QuickSort(T[] A,
+			int p, int r) {
+		if (p < r) {
+			int q = Partition(A, p, r);
+			if (q > p)
+				QuickSort(A, p, q - 1);
+			if (q < r - 1)
+				QuickSort(A, q + 1, r);
+		}
+	}
+
+	public static <T extends Comparable<? super T>> int Partition(T[] A, int p,
+			int r) {
+		Random random = new Random();
+		int i = Math.abs(random.nextInt(r - p) + p);
+
+		T temp = A[i];
+		A[i] = A[r];
+		A[r] = temp;
+		T pivot = A[r];
+		i = p - 1;
+
+		for (int j = p; j <= r - 1; j++) {
+			if (A[j].compareTo(pivot) <= 0) {
+				i++;
+				temp = A[i];
+				A[i] = A[j];
+				A[j] = temp;
+			}
+		}
+		temp = A[i + 1];
+		A[i + 1] = A[r];
+		A[r] = temp;
+
+		return i + 1;
+	}
+
+	public static void main(String args[]) throws IOException {
+		Integer[] arr = new Integer[n];
+		Integer[] auxi = new Integer[n];
+		Integer[] arrCopy = new Integer[n];
+		Integer[] arrCopyInsert = new Integer[n];
+		String folderpath = args[0];
+		// File path of training and test files
+				File trainingpath = new File(folderpath + "/out.txt");
+				BufferedReader br = null;
+				String line = null;
+				String[] data=null;
+
+		System.out.println("***Compare Run time complexity***");
+		
+		try {
+			
+			// Access List of files one after other from Ham folder
+			br = new BufferedReader(new FileReader(trainingpath));
+			while ((line = br.readLine()) != null) {
+				data = line.split(",");
+		}
+		}catch (FileNotFoundException e) // Handle File not Found error
+		{
+			System.out
+					.println("Please make sure the directory file is actually there.");
+		}
+		
+			for(int i=0;i<n;i++)
+			{
+			arr[i] = Integer.parseInt(data[i]);
+			arrCopy[i] = new Integer(arr[i]);
+			arrCopyInsert[i] = new Integer(arr[i]);
+			
+			}
+
+		// Merge Sort
+		System.out.println("Merge Sort");
+		StartTimer();
+		MergeSort(arr, 0, n - 1, auxi);
+		StopTimer();
+
+		// Quick Sort
+		System.out.println("Quick Sort");
+		StartTimer();
+		QuickSort(arrCopy, 0, n - 1);
+		StopTimer();
+
+		// Insertion Sort
+		System.out.println("Insertion Sort");
+		StartTimer();
+		InsertionSort(arrCopyInsert, 0, n - 1);
+		StopTimer();
+
+	}
+
+	private static void StopTimer() {
+		long stopTime = System.currentTimeMillis();
+		System.out.println("Total time : " + (stopTime - startTime) + " msec.");
+
+	}
+
+	private static void StartTimer() {
+		startTime = System.currentTimeMillis();
+	}
+}
---
+++ ReadMe.txt
@@ -0,0 +1,41 @@
+
+//Vaishakhi Kulkarni
+//vpk140230
+
+Input:Comparison of sorting algorithms: Merge sort vs Quick sort
+vs some O(n^2) algorithm for sorting (e.g., Insertion sort, Bubble sort). 
+
+Step:
+//The elements in the array are random
+1.Compile the Assignment5_a.java file
+2.Run with command java Assignment5_a
+
+Array size:1000000
+
+Output:
+***Compare Run time complexity***
+Merge Sort
+Total time : 787 msec.
+Quick Sort
+Total time : 750 msec.
+Insertion Sort
+Total time : 45022362 msec.
+
+Note:Time to execute Insertion sort is too much.
+
+// The element in the array are from specific file
+
+1.Compile the Assignment5_aF.java file
+2.Run with command java Assignment5_aF out.txt
+Specify the input file path
+Array size:8000000
+
+Output:
+***Compare Run time complexity***
+Merge Sort
+Total time : 8089 msec.
+Quick Sort
+Total time : 9523 msec.
+Insertion Sort
+
+Note:Time to execute Insertion sort is infinity.
